9552f7801ac9d061abf0ff0bd3000f81286c3fa6
Creed Petitt <creedpetitt@gmail.com>
2026-01-05 23:27:49 -0600

    define Interpreter and RuntimeError

---
+++ src/Interpreter/Interpreter.h
@@ -0,0 +1,33 @@
+//
+// Created by creed on 1/5/26.
+//
+
+#ifndef CIPR_INTERPRETER_H
+#define CIPR_INTERPRETER_H
+
+#include "../AST/Node/Node.h"
+#include <iostream>
+
+class Interpreter {
+public:
+    explicit Interpreter(Arena& arena) : arena(arena) {}
+
+    void interpret(int rootIndex);
+
+private:
+    Arena& arena;
+    Literal evaluate(int index);
+
+    static Literal visitLiteral(const Node& node);
+    Literal visitUnary(const Node& node);
+    Literal visitBinary(const Node& node);
+    Literal visitGrouping(const Node& node);
+
+    static bool isTruthy(const Literal& value) ;
+    static bool isEqual(const Literal& a, const Literal& b);
+    static void checkNumberOperand(const Token& op, const Literal& operand);
+    static void checkNumberOperands(const Token& op, const Literal& left, const Literal& right);
+    static std::string stringify(const Literal& value) ;
+};
+
+#endif //CIPR_INTERPRETER_H
